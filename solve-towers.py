631cf99e2d5328288d499dd434b6cb30fb033c9c
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2021-03-29 21:22:09 +0200

    Fix: bug-empty-generator bug-constant, Add instance constraints

--- solve-towers.py
+++ solve-towers.py
@@ -3,7 +3,7 @@ import itertools
 from collections import defaultdict
 
 #transpose a square matrix
-transpose = lambda m: zip(*m)
+transpose = lambda m: list(zip(*m))
 
 def IntSquareMatrix(prefix, sz):
     res = [[ Int(f'{prefix}_{i}_{j}') for i in range(sz)]
@@ -35,30 +35,46 @@ def gen_knowl_dict(n):
         knowl_dict[nb_towers].append(perm)
     return knowl_dict
 
-def solve_tower_puzzle(n, top, left, right, bottom):
+def solve_tower_puzzle(n, top, left, right, bottom, instance=None):
     knowl = gen_knowl_dict(n)
-    X = IntSquareMatrix('h', 5)
-
-    left_c = [constrain_towers(row, h, knowl)
-            for row, h in zip(X, left) if h > 0]
-    right_c = [constrain_towers(row[::-1], h, knowl)
-            for row, h in zip(X, right) if h > 0]
-
+    X = IntSquareMatrix('h', n)
     X_trans = transpose(X)
-    top_c = [constrain_towers(row, h, knowl)
-            for row, h in zip(X_trans, top) if h > 0]
-    bottom_c = [constrain_towers(row[::-1], h, knowl)
-            for row, h in zip(X_trans, bottom) if h > 0]
+
+    assert len(top) == len(left) == n
+    assert len(right) == len(bottom) == n
+
+    heights = itertools.chain(*X)
+    heights_c = [ And(h >= 1, h <= n) for h in heights ]
+
+    row_c = [ Distinct(row) for row in X ]
+    col_c = [ Distinct(row) for row in X_trans ]
+
+    left_c = [ constrain_towers(row, h, knowl)
+            for row, h in zip(X, left) if h > 0 ]
+    right_c = [ constrain_towers(row[::-1], h, knowl)
+            for row, h in zip(X, right) if h > 0 ]
+
+    top_c = [ constrain_towers(row, h, knowl)
+            for row, h in zip(X_trans, top) if h > 0 ]
+    bottom_c = [ constrain_towers(row[::-1], h, knowl)
+            for row, h in zip(X_trans, bottom) if h > 0 ]
 
     s = Solver()
-    s.add(left_c + right_c + top_c + bottom_c)
+    s.add(heights_c + row_c + col_c +
+           left_c + right_c + top_c + bottom_c)
+
+    if instance is not None:
+        for row_v, row in zip(X, instance):
+            for var, value in zip(row_v, row):
+                if value > 0:
+                    s.add(var == value)
 
     s.check()
     m = s.model()
 
     res = [[ m[s] for s in row] for row in X]
     return res
-    
+
 
 
 if __name__ == "__main__":
@@ -74,6 +90,31 @@ if __name__ == "__main__":
     solve_tower_puzzle(**puzzle_easy)
 
     #put -1 or 0 when number of towers seen is not available
+    puzzle_hard = {
+            'n': 5,
+            'top': [0, 2, 3, 0, 3],
+            'left': [0, 0, 0, 2, 3],
+            'right': [0, 0, 0, 0, 3],
+            'bottom': [0, 0, 0, 2, 3],
+            'instance': ((0,0,0,0,0),
+                (0,0,0,0,0),
+                (0,0,0,0,0),
+                (0,0,0,0,0),
+                (0,0,0,2,0)),
+    }
+    solve_tower_puzzle(**puzzle_hard)
 
-    #for difficult_puzzles
-    #FIXME: add possiblity to add the value of some heights (a little like the sudoku instance_c as in the official example)
+    puzzle_extreme = {
+            'n': 6,
+            'top': [2, 0, 0, 2, 3, 0],
+            'left': [0, 0, 2, 4, 3, 0],
+            'right': [3, 3, 2, 2, 0, 3],
+            'bottom': [3, 3, 0, 4, 0, 0],
+            'instance': ((0,0,0,0,0,0),
+                (0,0,0,0,0,0),
+                (0,0,0,0,0,0),
+                (0,0,1,0,0,0),
+                (0,0,0,0,0,0),
+                (0,0,0,0,0,0)),
+    }
+    solve_tower_puzzle(**puzzle_extreme)
